22293642eb6efd53d706cc02e640f1a9c3c5a4c8
Albaraa Mishlawi <albaraa_m@live.com>
2018-12-08 13:34:52 +0200

    implement basic server with routing

--- index.js
+++ index.js
@@ -2,3 +2,82 @@
  * Primary file for the API
  */
 
+// Dependencies
+const url = require('url');
+const http = require('http');
+const { StringDecoder } = require('string_decoder');
+
+const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  const { headers } = req;
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Choose the right handler
+    const chosenHandler = (typeof router[trimmedPath] !== 'undefined') ? router[trimmedPath] : handlers.notFound;
+
+    // construct data object
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer,
+    };
+
+    chosenHandler(data, (statusCode, payload) => {
+      // Set default values
+      statusCode = (typeof statusCode === 'number') ? statusCode : 200;
+      payload = (typeof payload === 'object') ? payload : {};
+      const payloadString = JSON.stringify(payload);
+
+      // return response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log('Returning this response: ', statusCode, payloadString);
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log('The server now listening to port 3000');
+});
+
+// Define handlers
+let handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {
+  callback(406, { name: 'Sample handler' });
+};
+// Not Found handler
+handlers.notFound = (data, callback) => {
+  callback(404);
+};
+
+// Define a request router
+const router = {
+  sample: handlers.sample,
+}
