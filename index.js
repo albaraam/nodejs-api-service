484cdf761f151f84d3eb3274dc9a813548c52578
Albaraa Mishlawi <albaraa_m@live.com>
2018-12-10 20:46:21 +0200

    moved the unified server to lib

--- index.js
+++ index.js
@@ -3,13 +3,11 @@
  */
 
 // Dependencies
-const url = require('url');
 const http = require('http');
 const https = require('https');
-const { StringDecoder } = require('string_decoder');
 const fs = require('fs');
 const config = require('./config');
-const router = require('./lib/router');
+const unifiedServer = require('./lib/UnifiedServer');
 
 // Instantiate http server
 const httpServer = http.createServer((req, res) => {
@@ -32,58 +30,3 @@ const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
 httpsServer.listen(config.httpsPort, () => {
   console.log(`The server now listening to port ${config.httpsPort} in ${config.envName} mode`);
 });
-
-// All unified logic for http & https servers
-const unifiedServer = (req, res) => {
-  // Get the URL and parse it
-  const parsedUrl = url.parse(req.url, true);
-
-  // Get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-  // Get the query string as an object
-  const queryStringObject = parsedUrl.query;
-
-  // Get the HTTP method
-  const method = req.method.toLowerCase();
-
-  // Get the headers as an object
-  const { headers } = req;
-
-  // Get the payload, if any
-  const decoder = new StringDecoder('utf-8');
-  let buffer = '';
-  req.on('data', (data) => {
-    buffer += decoder.write(data);
-  });
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    // Choose the right handler
-    const chosenHandler = (typeof router[trimmedPath] !== 'undefined') ? router[trimmedPath] : router.notFound;
-
-    // construct data object
-    const data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: buffer,
-    };
-
-    chosenHandler(data, (statusCode, payload) => {
-      // Set default values
-      statusCode = (typeof statusCode === 'number') ? statusCode : 200;
-      payload = (typeof payload === 'object') ? payload : {};
-      const payloadString = JSON.stringify(payload);
-
-      // return response
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      console.log('Returning this response: ', statusCode, payloadString);
-    });
-  });
-};
